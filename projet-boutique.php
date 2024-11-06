3b49eadb2f80ade1ff8f6f5027c8829a141ed465
Ozil3920 <omerozil3920@gmail.com>
2024-11-06 11:03:31 +0100

    Update projet-boutique.php

--- projet-boutique.php
+++ projet-boutique.php
@@ -3,7 +3,7 @@
 // Initialisation du stock
 
 $articles = [" Chaussettes", " T-shirts", " Chaussures", " Casquettes", " Robes"];
-$quantites = [10, 5, 8, 3, 12];
+$quantites = [24, 15, 7, 4, 2];
 $ventes = [0, 0, 0, 0, 0];
 
 // Initialisation de la variable "choix"
